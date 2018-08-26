58315e99d8501e3ade75f6e13b8bd943aad370fe
qinggge <zhuhuang@126.com>
2018-08-26 23:05:53 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="header">
+        <img src="../assets/logo.svg" alt="">
+        <ul>
+            <li><a href="#">首页</a></li>
+            <li><a href="#">新手入门</a></li>
+            <li><a href="#">API</a></li>
+            <li><a href="#">关于</a></li>
+            <li><a href="#">注册</a></li>
+            <li><a href="#">登录</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+a{
+    text-decoration: none;
+    color: #CCC;
+    font-size: 14px;
+    text-shadow: none;
+}
+.header{
+    background-color: #444;
+    height: 50px;
+}
+img{
+    max-width: 120px;
+    margin-left: 50px;
+    margin-top: 10px;
+}
+ul{
+    list-style: none;
+    float: right;
+    margin: 4px;
+}
+ul li{
+    display: inline-block;
+    padding: 10px 15px;
+}
+</style>
